6f440d24fb7beec46f59afd1d78bbd13e1ff7671
yoon <luffy1021@naver.com>
2017-05-14 19:42:13 +0900

    이미지 스크롤 뷰

--- YSImageScroll/YSImageScroll/ViewController.swift
+++ YSImageScroll/YSImageScroll/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var verticalStackView: UIStackView!
+    
+    var imageArray = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageArray = [#imageLiteral(resourceName: "001"), #imageLiteral(resourceName: "003"), #imageLiteral(resourceName: "002")]
+        
+        scrollViewImages()
     }
-
+    
+    func scrollViewImages() {
+        for i in 0..<imageArray.count {
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            imageView.contentMode = .scaleToFill
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.scrollView.frame.width, height: self.scrollView.frame.height)
+            
+            scrollView.contentSize.width = scrollView.frame.size.width * CGFloat(i + 1)
+            scrollView.addSubview(imageView)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
